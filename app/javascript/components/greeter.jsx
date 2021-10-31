30d3c63885b017cccfe09bb07b666a6617d65093
Praveen Dhawan <praveen@pdhawan.com>
2021-10-31 11:23:40 +0530

    Make sign in and sign up work

--- app/javascript/components/greeter.jsx
+++ app/javascript/components/greeter.jsx
@@ -3,25 +3,39 @@ import ReactServer from 'react-dom/server'
 import ReactDOM from 'react-dom'
 
 let Greet = () => <h1>Hello, world!</h1>
-console.log(ReactServer.renderToString(<Greet />))
 
+container = document.getElementById('myapp')
 
-function formatName(user) {
-    return user.firstName + ' ' + user.lastName;
+props = {
+  email: container.getAttribute('data-user-email'),
+  signInUrl: container.getAttribute('data-signin-url'),
+  signUpUrl: container.getAttribute('data-signup-url'),
+  logOutUrl: container.getAttribute('data-logout-url')
 }
 
-const user = {
-    firstName: 'Praveen',
-    lastName: 'Dhawan'
-};
+function LoggedInGreeter(props) {
+  return <h1>
+    <span> Hello, {props.email}! </span>
+    <a data-method="delete" href={props.logOutUrl}>Log Out</a>
+  </h1>
+}
 
-const element = (
-    <h1>
-        Hello, {formatName(user)}!
-    </h1>
-);
+function GuestGreeter(props) {
+  return <h1>
+    <span> Hello, Guest! </span>
+    <a href={props.signInUrl}>Log In</a>
+    <a href={props.signUpUrl}>Sign Up</a>
+  </h1>
+}
+
+
+if (props.email) {
+  element = LoggedInGreeter(props)
+} else {
+  element = GuestGreeter(props)
+}
 
 ReactDOM.render(
     element,
-    document.getElementById('myapp')
+    container
 );
